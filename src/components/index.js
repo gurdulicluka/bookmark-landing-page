2d34ee91c5eb61ccd026fd560995fd85fccb8837
Luka Gurdulic <gurdulicluka@gmail.com>
2022-10-19 22:10:39 +0200

    BUILD(Folders): All components, layout

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Features from "./Features";
+import Extensions from "./Extensions";
+import FAQ from "./FAQ";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export { Navbar, Hero, Features, Extensions, FAQ, CTA, Footer };
