a2d5d88664dfcd36a534cc8e61fef24e83b2abfd
Luka Gurdulic <gurdulicluka@gmail.com>
2022-10-30 18:38:11 +0100

    FIX Button component: clashing styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  return <button className={props.style}>{props.children}</button>;
+  return <button className={`btn ${props.style} `}>{props.children}</button>;
 };
 
 export default Button;
