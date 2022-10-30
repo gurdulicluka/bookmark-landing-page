028ae929d5aed83cc8914af196b35834ab2d1f7e
Luka Gurdulic <gurdulicluka@gmail.com>
2022-10-30 18:39:42 +0100

    ADD Hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Hero.css";
+import hero from "../assets/illustration-hero.svg";
+import Button from "./Button";
+import "animate.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero__text">
+        <h1>
+          A Simple Bookmark <br /> Manager
+        </h1>
+        <p>
+          A clean and simple interface to organize your favorite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div className="hero__buttons">
+          <Button style="primary">Get it on Chrome</Button>
+          <Button style="secondary">Get it on Firefox</Button>
+        </div>
+      </div>
+      <div className="hero__illustration">
+        <img
+          src={hero}
+          alt="computer_screen"
+          className="animate__animated animate__backInRight"
+        />
+        <div className="bg-shape animate__animated animate__slideInRight" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
